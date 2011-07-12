13ac0f4320b2ce3265630df48a0c5b3d691e6e0d
SilverCreek <0xamlou@gmail.com>
2011-07-13 00:01:15 +0100

    first commit

---
+++ waarbot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waarbot::Application.config.secret_token = 'a6d9bbb9c0b7fb30139ccfc38b83837cb0f3cc0d07dc66b0f1cea0af3fd3a687710f570cbad360a426de859996fc64e599403704c3d360115c5d25e698966673'
